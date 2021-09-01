b2f92aee40a4cf8d0574fe7dfa84e5493221b709
manos00 <manos.wagner@gmail.com>
2021-09-01 18:35:09 +0200

    Changes to bashrc and config.fish

--- .bashrc
+++ .bashrc
@@ -5,7 +5,9 @@
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
+# Aliases
 alias ls='ls --color=auto'
 PS1='[\u@\h \W]\$ '
 
+# Variables
 export PATH="/usr/local/scripts/:$PATH"
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,18 @@
-# Put system-wide fish configuration entries here
-# or in .fish files in conf.d/
-# Files in conf.d can be overridden by the user
-# by files with the same name in $XDG_CONFIG_HOME/fish/conf.d
-
-# This file is run by all fish instances.
-# To include configuration only for login shells, use
 # if status is-login
 #    ...
 # end
-# To include configuration only for interactive shells, use
 # if status is-interactive
 #   ...
 # end
 
-alias icat="kitty +kitten icat"
+# ALIASES
+if string match -q "*kitty*" $TERM
+	alias icat="kitty +kitten icat"
+	alias kitty-update="curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin"
+end
+
+alias gitdot="/usr/bin/git --git-dir=$HOME/github/dotfiles/ --work-tree=$HOME"
+alias p="sudo pacman -S"
+
+# VARIABLES
 set PATH "$PATH:/home/manos/bin:/usr/local/scripts:/opt"
-alias kitty-update="curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin"
-alias up="cd .."
-alias config="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
