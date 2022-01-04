037c16b064e0dcf0afeb5532b08570baf2231db3
manos00 <manos.wagner@gmail.com>
2022-01-04 21:38:06 +0100

    finished install script

--- github/dotfiles/install.sh
+++ github/dotfiles/install.sh
@@ -4,6 +4,7 @@
 # - git
 
 
+# check if dotfiles folder exists already
 if [[ -d "$HOME/github/dotfiles" ]];then
 	echo "Directory $HOME/github/dotfiles already exists."
 	exit 1
@@ -11,24 +12,45 @@ else
 	LOG="/tmp/checkout-err.txt"
 	ERRORL1="error: The following untracked working tree files would be overwritten by checkout:"
 	ERRORLX="Please move or remove them before you switch branches."
-	#trap "rm -f $LOG" EXIT
+	trap "rm -f $LOG" EXIT
 	mkdir -p $HOME/github/dotfiles/ && git clone --bare https://github.com/manos00/dotfiles $HOME/github/dotfiles
 	conf="/bin/env git --git-dir=$HOME/github/dotfiles/ --work-tree=$HOME"
 	cd $HOME && $conf checkout 2> $LOG
 	if [[ $? == 1 ]];then
 		l=$(wc -l < $LOG)
 		if [[ $(sed '1q;d' $LOG) == $ERRORL1 ]];then
+			BADFILES=()
 			echo There are conflicting files:
 			for i in $(seq 2 $l);do
 				if [[ $(sed "${i}q;d" $LOG) != $ERRORLX ]];then
 					line="$(sed "${i}q;d" $LOG)"
-					echo $line | sed 's/ //g' 
+					echo $line | sed 's/ //g'
+					BADFILES+=$line
 				else
 					break
 				fi
 			done
+			# Choose which files to backup or delete
+			for file in ${BADFILES[@]};do
+				echo "Do you want to backup $file? (y/n)" 
+				read choice
+				if [[ $choice == "y" ]];then
+					backup=$HOME/dotfilesbackup
+					if [[ ! -d $backup ]];then	
+						mkdir $backup
+					fi
+					mv $file $backup
+				else
+					echo Aborting...
+					exit 0
+				fi
+ 			done
+			cd $HOME && $conf checkout 2> $LOG
+		else
+			echo An unknown error occured!
+			cat $LOG
+			exit 1
 		fi
-		exit 1
 	else
 		exit 0
 	fi
